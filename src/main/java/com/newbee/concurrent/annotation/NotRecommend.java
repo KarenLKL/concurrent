432dab1e5a1c4e5f49bebd34f637932f9ec2c506
likailun <likailun@bonc.com.cn>
2018-12-20 14:27:03 +0800

    线程安全与线程不安全相关注解

---
+++ src/main/java/com/newbee/concurrent/annotation/NotRecommend.java
@@ -0,0 +1,15 @@
+package com.newbee.concurrent.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标识【不推荐】的类或者方法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)//注解使用的范围
+public @interface NotRecommend {
+    String value() default "";
+}
